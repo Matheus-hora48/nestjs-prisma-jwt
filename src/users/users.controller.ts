71ef0404ca8264afce001c9fc90cd6a380c7d14d
Matheus Hora <matheus11hora@gmail.com>
2023-10-10 09:35:38 -0300

    feat: swagger nos users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,15 +2,23 @@ import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Request, Response } from 'express';
 import { JwtAuthGuard } from 'src/authentication/auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiTags('usuario')
+@ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Get()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Busca todos usuários do banco' })
+  @ApiResponse({ status: 200, description: 'Usuários encontrados' })
   async getAllUsers(
     @Req() request: Request,
     @Res() response: Response,
