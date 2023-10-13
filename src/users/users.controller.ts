afbb545dd11cc47dbc91366b6b18bfbd602be313
Matheus Hora <matheus11hora@gmail.com>
2023-10-13 15:03:30 -0300

    feat: colocando outras rotas para user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Req,
+  Res,
+  UseGuards,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Request, Response } from 'express';
 import { JwtAuthGuard } from 'src/authentication/auth.guard';
@@ -8,6 +18,7 @@ import {
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
+import { Users } from '@prisma/client';
 
 @ApiTags('usuario')
 @ApiBearerAuth()
@@ -34,6 +45,103 @@ export class UsersController {
       return response.status(500).json({
         status: 'Ok!',
         message: 'Internal Server Error!',
+      });
+    }
+  }
+
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Busca um usuário por ID' })
+  @ApiResponse({ status: 200, description: 'Usuário encontrado' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado' })
+  async getUserById(
+    @Param('id') id: number,
+    @Res() response: Response,
+  ): Promise<any> {
+    try {
+      const user = await this.userService.getUserById(id);
+
+      if (!user) {
+        return response.status(404).json({
+          status: 'Not Found',
+          message: 'User not found',
+        });
+      }
+
+      return response.status(200).json({
+        status: 'Ok!',
+        message: 'User found',
+        result: user,
+      });
+    } catch (err) {
+      return response.status(500).json({
+        status: 'Internal Server Error',
+        message: 'Internal Server Error',
+      });
+    }
+  }
+
+  @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Atualiza um usuário por ID' })
+  @ApiResponse({ status: 200, description: 'Usuário atualizado com sucesso' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado' })
+  async updateUser(
+    @Param('id') id: number,
+    @Body() data: Partial<Users>,
+    @Res() response: Response,
+  ): Promise<any> {
+    try {
+      const updatedUser = await this.userService.updateUser(id, data);
+
+      if (!updatedUser) {
+        return response.status(404).json({
+          status: 'Not Found',
+          message: 'User not found',
+        });
+      }
+
+      return response.status(200).json({
+        status: 'Ok!',
+        message: 'User updated successfully',
+        result: updatedUser,
+      });
+    } catch (err) {
+      return response.status(500).json({
+        status: 'Internal Server Error',
+        message: 'Internal Server Error',
+      });
+    }
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Exclui um usuário por ID' })
+  @ApiResponse({ status: 200, description: 'Usuário excluído com sucesso' })
+  @ApiResponse({ status: 404, description: 'Usuário não encontrado' })
+  async deleteUser(
+    @Param('id') id: number,
+    @Res() response: Response,
+  ): Promise<any> {
+    try {
+      const deletedUser = await this.userService.deleteUser(id);
+
+      if (!deletedUser) {
+        return response.status(404).json({
+          status: 'Not Found',
+          message: 'User not found',
+        });
+      }
+
+      return response.status(200).json({
+        status: 'Ok!',
+        message: 'User deleted successfully',
+        result: deletedUser,
+      });
+    } catch (err) {
+      return response.status(500).json({
+        status: 'Internal Server Error',
+        message: 'Internal Server Error',
       });
     }
   }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,31 +1,52 @@
-import { PrismaService } from "src/prisma.service";
-import { Users } from "./users.model";
-import { ConflictException, Injectable } from "@nestjs/common";
-
+import { PrismaService } from 'src/prisma.service';
+import { Users } from './users.model';
+import { ConflictException, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService{
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
 
-     constructor(private prisma: PrismaService){}
+  async getAllUser(): Promise<Users[]> {
+    return this.prisma.users.findMany();
+  }
 
-     async getAllUser():Promise<Users[]>{
-          return this.prisma.users.findMany()
-     }
+  async createUser(data: Users): Promise<Users> {
+    const existing = await this.prisma.users.findUnique({
+      where: {
+        cnpj: data.cnpj,
+      },
+    });
 
+    if (existing) {
+      throw new ConflictException('username already exists');
+    }
 
-     async createUser(data:Users): Promise<Users>{
-          const existing = await this.prisma.users.findUnique({
-               where: {
-                 cnpj: data.cnpj,
-               },
-             });
-         
-             if (existing) {
-               throw new ConflictException('username already exists');
-             }
-         
-             return this.prisma.users.create({
-               data,
-             });
-     }
+    return this.prisma.users.create({
+      data,
+    });
+  }
+  async getUserById(id: number): Promise<Users | null> {
+    return this.prisma.users.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async updateUser(id: number, data: Partial<Users>): Promise<Users | null> {
+    return this.prisma.users.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  async deleteUser(id: number): Promise<Users | null> {
+    return this.prisma.users.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
